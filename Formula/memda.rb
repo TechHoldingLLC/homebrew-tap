10057d55d49fffd536edae0412d3a60132716f09
Yorick Demichelis <yorick.demichelis@techholding.co>
2021-03-10 09:56:22 -0800

    feat: update memda version and sha256

--- Formula/memda.rb
+++ Formula/memda.rb
@@ -1,10 +1,11 @@
 class Memda < Formula
-  VERSION = "v1.2.0".freeze
+  VERSION = "v1.3.0".freeze
 
   # rubocop:disable Layout/HashAlignment
   SUMS = Hash[
-    "darwin-amd64"  => "cf8ef4f5343fb3a878b16ce7b444ee8dccea49f7c1a2b5f9ad1e507552fffe2d",
-    "linux-amd64"   => "62a681f53b031bc20a6319575595e754b6d84a8c12154763cd159f72c712eec3"
+    "darwin-amd64"  => "827ed60e3b2d258c6065dcf29e7dd4ab67e4216636a40e5f2fc40bfaef3faa41",
+    "darwin-arm64"  => "ef3b75e713118d46788f31fe6e16e4ba9d6da304b1755ab31e7d8f55f344b34f",
+    "linux-amd64"   => "ccb9ef958bd6b51337e0dfb7c3fde0ab889ebd7c4178e7cd20770066ca21dee6"
   ].freeze
   # rubocop:enable Layout/HashAlignment
   RAW_OS = `uname -s`.freeze
@@ -14,12 +15,12 @@ class Memda < Formula
     raise 'Unsupported OS - please check release page'
   end
   
-  unless RAW_ARCH == "x86_64\n"
+  unless RAW_ARCH == "x86_64\n" || RAW_ARCH == "arm64\n"
     raise 'Unsupported architecture - please check release page'
   end
 
   OS = (RAW_OS == "Darwin\n" ? "darwin" : "linux").freeze
-  ARCH = "amd64".freeze
+  ARCH = (RAW_OS == "x86_64\n" ? "amd64" : "arm64").freeze
 
   SHA256 = SUMS["#{OS}-#{ARCH}"].freeze
 
