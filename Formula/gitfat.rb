25eb341ccf883c037bcda5bd13bf0e5572920199
Yorick Demichelis <yorick@techholding.co>
2022-01-18 16:20:33 -0800

    fix: missing comma

--- Formula/gitfat.rb
+++ Formula/gitfat.rb
@@ -5,7 +5,7 @@ class Memda < Formula
   SUMS = Hash[
     "darwin-amd64"  => "62a991c80f06165e6c52febd2774f17ed775e4a4de6b108b5a8313da2cc06f61",
     "darwin-arm64"  => "b4031470b707548bd8668a38a03df8519afac76c9961bfb8e01fb8764a6539e9",
-    "linux-amd64"   => "a84fa326b13994e0865c1d29dd0da975de9f19d7e0c384aa29807be39a65370a"
+    "linux-amd64"   => "a84fa326b13994e0865c1d29dd0da975de9f19d7e0c384aa29807be39a65370a",
     "linux-arm64"   => "9bda99da8c40ac4831b28ef5ef2ee077d7bfc6f00f84e2c59c400bb5e22bc811"
   ].freeze
   # rubocop:enable Layout/HashAlignment
